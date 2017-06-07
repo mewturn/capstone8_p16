afd37d7fa29c0d6e7ba6edec067d3a8e0e2fe41c
mewturn <pathonice@hotmail.com>
2017-06-07 14:53:33 +0800

    Changed image input source
    Image file's source is now taken as an argument from the command line

--- imageAnalysis.py
+++ imageAnalysis.py
@@ -1,28 +1,38 @@
 from PIL import Image
+import sys
 
-# Change the directory of the image file here
-file = "sample.jpg"
-
-# Opens the file and reads the data pixel by pixel, then puts each pixel into a list
+## Takes the file name as the first argument
+file = sys.argv[1]
 im = Image.open(file)
+
+## Converts .png to .jpg if the image is in .png
+## Reopens the file afterwards
+if file.lower().endswith('.png'):
+    rgb_im = im.convert('RGB')
+    file = file[:-4] + '.jpg'
+    rgb_im.save(file)
+    im = Image.open(file)
+    
+## Reads the data pixel by pixel, then puts each pixel into a list
 pixels = list(im.getdata())
 
-# Initialize variables: green pixels, non-green pixels and total pixels	
+## Initialize variables: green pixels, non-green pixels and total pixels	
 green = 0
 other = 0
 totalpixels = 0
 
-# Do for each pixel in the image
-# Each pixel is a 3-tuple -> pixel[0]: R-value, pixel[1]: G-value, pixel[2]: B-value
+## Do for each pixel in the image
+## Each pixel is a 3-tuple -> pixel[0]: R-value, pixel[1]: G-value, pixel[2]: B-value
 for pixel in pixels:
-	if pixel[1] > pixel[0] and pixel[1] > pixel[2]:	# Green, if G-value > R-value AND G-value > B-value
-		green += 1
-	else:											# Not green, otherwise
-		other += 1 
+    print(pixel)
+    if pixel[1] > pixel[0] and pixel[1] > pixel[2]:	# Green, if G-value > R-value AND G-value > B-value
+        green += 1
+    else:											# Not green, otherwise
+        other += 1 
 
-# Calculate total pixels in the image
+## Calculate total pixels in the image
 totalpixels = green + other
 	
-# Outputs
+## Outputs
 print('Green: ' + str(green)+', Other: '+ str(other) + ', Total: ' + str(totalpixels))
 print('Percentage of Green: ',(100 * green/totalpixels), '%') 
