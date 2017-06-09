b298fc2f5e9c94960af669a9f54e099edba5c671
mewturn <pathonice@hotmail.com>
2017-06-09 11:35:44 +0800

    Several major algorithm fixes
    1. Uses linear regression function to calculate f(x)
    2. Fixed a critical sign error which caused gradient to increase
    unexpectedly
    3. Set max-iterations to 10000 and checkpoints to every 2000 iterations
    4. Included potential for calculating validation error using some test
    data sets

--- trainingData.py
+++ trainingData.py
@@ -1,3 +1,9 @@
+## Milton Li 2017
+## Capstone 8 - Project 16: Bio-waste Disposal
+## Singapore University of Technology and Design
+
+## Uses simple linear regression to determine if a pixel is green or non-green
+
 from PIL import Image
 import sys
 import numpy as np
@@ -9,37 +15,44 @@ from imageAnalysis import getPixelData
 
 ## totalimages: number of total images
 ## type: type of image -> "green" or "notgreen"
-def run(totalimages, type):
+def run(totalimages, type=None):
     ## Output to be returned at the end
-    output = []
+    output = []        
+    count = 0
     
     ## Gets the pixel data from the image
     for i in range(1, totalimages+1):
         image = type + str(i) + ".jpg"
         pixels = getPixelData(image)
-            
+        print("Processing image...")
+        
         ## Adds it to the array of green output
         for pixel in pixels:
             if pixel in output:
                 continue
+                
             output.append(pixel)
-    
+        print("Complete!")
     return output
 
-def train(trainingdata, weights, ground_truth):   
+def train(trainingdata, weights, ground_truth, testingdata, test_ground_truth):   
     ## Learning rate and the expected change to last (w(t+1) - w(t))
-    lr = 0.05
-    eps = 0.000001
+    lr = 0.005
+    eps = 1e-6
     
     ## Initialize a temporary variable to check if the expected stepsize is reached
     change = 1
     
     ## Sets the iteration count and the maximum iterations allowed
     iteration = 0
-    maxitr = 2000   
+    maxitr = 10000   
     
-    ## Normalise the training data w.r.t to maximum RGB value of 255
+    ## Normalise the datasets w.r.t to maximum RGB value of 255
     trainingdata = np.divide(trainingdata, 255.0)
+    testingdata = np.divide(testingdata, 255.0)
+    
+    ## N: the size of training data
+    N = len(trainingdata)
     
     ## Keep changing weights until the required step size (eps)
     while change > eps:      
@@ -50,20 +63,29 @@ def train(trainingdata, weights, ground_truth):
         
         ## Keeps track of iteration number and allows us to know that the program has not hung
         iteration += 1
-        
-        ## The size of data: N
-        N = len(trainingdata)
-        
+
         ## Modify the weights according to the classifications
-        for i in range(N):            
+        for i in range(N):         
+            ## Predict f(x)
             predict = activation(trainingdata[i], weights)
+            
+            ## Compute error for the particular x in the training data
             error += (computeError(ground_truth[i], predict)) / N
+            
+            ## Compute gradient for the particular x in the training data w.r.t. w
             update += (computeGradient(ground_truth[i], predict, trainingdata[i])) / N                      
         
-            weights -= lr * update
-            change = np.linalg.norm(update)
+        ## Update weights
+        weights -= lr * update
+        
+        ## Calculate norm(w(t+1) - w(t)) 
+        change = np.linalg.norm(lr * update)
           
-        print("Iteration #", iteration, "Error:", error, "Weight:", weights)
+        ## Intervals to checkpoint 
+        if iteration % 200 == 0:
+            print("Iteration #", iteration, "Error:", error, "Weight:", weights)            
+            ## Check validation error with some set of testing data
+            #print("Validation error:", test(testingdata, weights, test_ground_truth))
         
         ## End when the expected step size is achieved (eps) or when the maximum iteration is reached    
         if iteration > maxitr:
@@ -78,10 +100,11 @@ def train(trainingdata, weights, ground_truth):
 def test(testingdata, weights, ground_truth):    
     ## Initialise error
     error = 0
+    N = len(testingdata)
     
-    for data in testingdata:
+    for i in range(N):
         predict = activation(testingdata[i], weights)
-        error += computeError(predict, ground_truth[i])
+        error += computeError(ground_truth[i], predict)
     
     return error
     
@@ -95,8 +118,7 @@ def computeError(actual, predict):
 
 ## We compute gradient of the error function w.r.t. w: (np.dot(w,x) - y) times x
 def computeGradient(actual, predict, parameters):    
-    print(2 * (actual - predict) * parameters)
-    return 2 * (actual - predict) * parameters
+    return -2 * (actual - predict) * parameters
     
     
 ## Generates a list of ground truths comprising 1 (green) or -1 (not green)
@@ -109,8 +131,8 @@ def groundTruth(ones, negs):
 if __name__ == "__main__":
     ## __images: number of images in the training set
     ## __output: Output list to write into, without duplicate RGB values
-    greenimages = 1     ## Currently up to 4
-    otherimages = 1     ## Currently up to 3
+    greenimages = 5     ## Currently up to 5
+    otherimages = 5     ## Currently up to 5
     greenoutput = run(greenimages, "green")
     otheroutput = run(otherimages, "notgreen")
     
@@ -126,14 +148,25 @@ if __name__ == "__main__":
     # WEIGHT TRAINING
     ## Initialise weights to be [w(0), w(1), w(2), w(3)] = [0, 0, 0, 0]
     w = np.zeros(4)
+
+    '''    WEIGHTS AT DIFFERENT ITERATION NUMBERS (INTERVALS OF 2000)  
+    ## ITR 2000 ->  Error: 0.29987, Weights: [0.027571, 0.10667, 1.7119, -0.15630]
+    ## ITR 4000 ->  Error: 0.24196, Weights: [0.043756, -0.34832, 2.1669, -0.55597]
+    ## ITR 6000 ->  Error: 0.21088, Weights: [0.057828, -0.70851, 2.5007, -0.81439]
+    ## ITR 8000 ->  Error: 0.19369, Weights: [0.070381, -0.99738, 2.74890, -0.97368]
+    ## ITR 10000 -> Error: 0.18378, Weights: [0.081832, -1.2328, 2.9351, -1.0643]
     '''
-    ## After 5,000 iterations: w: [-36.987, -0.387, 0.275, -0.281]
-    w = np.asarray([-36.987, -0.387, 0.275, -0.281])
     
+    ## WEIGHT TESTING
+    testingoutput = getPixelData("test_green.jpg")
     
-    ## After 10,000 iterations: w: [-65.377, -0.267, 0.379, -0.388]
-    w = np.asarray([-65.377, -0.267, 0.379, -0.388])
-    '''
+    ## Creates an array of testing data and adds a weight bias term 1 to the front of each x vector
+    testingset = np.asarray(testingoutput)
+    testingset = np.insert(testingset, 0, 1, axis=1)   
+    
+    ## Ground truths of test data
+    testgroundtruths = groundTruth(len(testingoutput), 0)
+    
     ## Commence training
-    w = train(trainingset, w, groundtruths)
+    w = train(trainingset, w, groundtruths, testingset, testgroundtruths)
     
