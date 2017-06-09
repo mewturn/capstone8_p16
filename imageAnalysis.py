e94fd33fdeaad1e4b95e1e9a8610ac60ebccc64d
mewturn <pathonice@hotmail.com>
2017-06-09 12:11:37 +0800

    Modified weights and threshold
    Changed threshold score to ">1" and modified the weights to fit the new
    weights output by the training datasets. Also added a progress tracker
    in cases of large datasets so users would know that the program is still
    running.

--- imageAnalysis.py
+++ imageAnalysis.py
@@ -29,22 +29,40 @@ def calculateRGB(pixels):
     ## Initialize variables: green pixels, non-green pixels and total pixels	
     green = 0
     other = 0
-    totalpixels = 0
-
+    processedpixels = 0
+    totalpixels = len(pixels)
+    
+    ## Completion iterator and update interval to keep track of progress
+    i = 1
+    interval = 10.0
+    
+    print("Processing...")
+        
     ## Do for each pixel in the image
     ## Each pixel is a 3-tuple -> pixel[0]: R-value, pixel[1]: G-value, pixel[2]: B-value
     for pixel in pixels:      
-    
-        ## Current Classification Method -> w: [0.081832, -1.2328, 2.9351, -1.0643]
+        processedpixels += 1  
+              
+        ## Prepare an array of pixels and normalise w.r.t. the maximum value of RGB: 255.0
         pixelarray = np.insert(np.asarray(pixel), 0, 1)
+        pixelarray = np.divide(pixelarray, 255.0)
+        
+        ## Current Classification Method -> w: [0.081832, -1.2328, 2.9351, -1.0643]
         weights = np.asarray([0.081832, -1.2328, 2.9351, -1.0643])
         
+        ## Classifier: f(x) = w <dot> x
         score = np.dot(pixelarray, weights)
         
-        if score > 0:
+        ## If score is more than 1, we are confident that it is a green pixel
+        if score > 1:
             green += 1
         else:
             other += 1
+        
+        ## Keeping track of the progress    
+        if (100 * processedpixels/totalpixels >= i * interval):
+            print(100 * processedpixels/totalpixels, "% complete")  
+            i += 1
             
         ''' Old Classification Method 
         if pixel[1] > pixel[0] and pixel[1] > pixel[2]:	# Green, if G-value > R-value AND G-value > B-value
@@ -52,8 +70,6 @@ def calculateRGB(pixels):
         else:											# Not green, otherwise
             other += 1 
         '''
-    ## Calculate total pixels in the image
-    totalpixels = green + other
     
     ## Green pixel ratio
     greenratio = (100 * green/totalpixels)
@@ -67,8 +83,7 @@ def calculateRGB(pixels):
 if __name__ == "__main__":
     ## Takes the file name as the first argument
     file = sys.argv[1]
-    calculateRGB(getPixelData(file))
-    
+    calculateRGB(getPixelData(file))    
     
     ## Data Tabulation
     ## green1.jpg -> Green: 16,622, Non-green: 1,067, Percentage of Green: 94.0%
